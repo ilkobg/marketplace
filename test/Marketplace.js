93dc3a893fea169f81f43f096b550c7fc79f2b02
i.iliev <i.iliev@allterco.com>
2023-07-04 22:57:50 +0300

    Organise tests and cover the case of listing NFT with not the owner

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -29,133 +29,144 @@ describe("Marketplace", function () {
     await nftContract.safeMint(marketplace.address, owner.address);
   });
 
-  it("Should list an NFT for sale", async function () {
-    await marketplace
+  describe("List", function () {
+    it("Should list an NFT for sale", async function () {
+      await marketplace
+        .connect(owner)
+        .listNFT(tokenId, price, nftContractAddress);
+  
+      const listing = await marketplace.listings(nftContractAddress, tokenId);
+      expect(listing.owner).to.equal(owner.address);
+      expect(listing.price).to.equal(price);
+  
+      expect(await nftContract.ownerOf(tokenId)).to.equal(owner.address);
+  
+      const filter = marketplace.filters.List();
+      const events = await marketplace.queryFilter(filter);
+  
+      expect(events.length).to.equal(1);
+    });
+  
+    it("Should throw when listing an NFT that is already listed", async function () {
+      await marketplace
+        .connect(owner)
+        .listNFT(tokenId, price, nftContractAddress);
+  
+      const listing = await marketplace.listings(nftContractAddress, tokenId);
+      expect(listing.owner).to.equal(owner.address);
+      expect(listing.price).to.equal(price);
+  
+      expect(await nftContract.ownerOf(tokenId)).to.equal(owner.address);
+  
+      expect(
+        marketplace.connect(owner).listNFT(tokenId, price, nftContractAddress)
+      ).to.be.revertedWith("NFT is already listed");
+    });
+  
+    it("Should throw when trying to list NFT with not the owner", async function () {
+      await expect(
+        marketplace.connect(buyer).listNFT(tokenId, price, nftContractAddress)
+      ).to.be.revertedWith("Only owner can list the NFT for sale");
+    });
+  
+    it("Should throw when listing an NFT with zero price", async function () {
+      expect(
+        marketplace.connect(owner).listNFT(tokenId, 0, nftContractAddress)
+      ).to.be.revertedWith("Price must be greater than zero");
+    });
+  });
+
+  describe("Buy", function () {
+    it("Should buy an NFT", async function () {
+      await marketplace
+        .connect(owner)
+        .listNFT(tokenId, price, nftContractAddress);
+  
+      const initialBalanceSeller = await ethers.provider.getBalance(
+        owner.address
+      );
+      const initialBalanceBuyer = await ethers.provider.getBalance(buyer.address);
+  
+      const transaction = await marketplace
+        .connect(buyer)
+        .buyNFT(tokenId, nftContractAddress, { value: price });
+      const receipt = await transaction.wait();
+  
+      const gasUsed = receipt.gasUsed;
+      const gasPrice = transaction.gasPrice;
+      const gasFee = gasUsed.mul(gasPrice);
+  
+      const updatedBalanceSeller = await ethers.provider.getBalance(
+        owner.address
+      );
+      const updatedBalanceBuyer = await ethers.provider.getBalance(buyer.address);
+  
+      expect(updatedBalanceSeller).to.equal(initialBalanceSeller.add(price));
+      expect(updatedBalanceBuyer).to.equal(
+        initialBalanceBuyer.sub(price).sub(gasFee)
+      );
+  
+      expect(await nftContract.ownerOf(tokenId)).to.equal(buyer.address);
+  
+      const filter = marketplace.filters.Buy();
+      const events = await marketplace.queryFilter(filter);
+  
+      expect(events.length).to.equal(1);
+    });
+  
+    it("Should throw when trying to buy NFT that is not listed", async function () {
+      await expect(
+        marketplace.connect(buyer).buyNFT(tokenId, nftContractAddress, { value: price })
+      ).to.be.revertedWith("Token is not listed");
+    });
+  
+    it("Should throw when trying to buy NFT with unsufficient funds", async function () {
+      await marketplace
+        .connect(owner)
+        .listNFT(tokenId, price, nftContractAddress);
+  
+      const smallerPrice = price.sub(ethers.utils.parseEther("0.1"));
+  
+      await expect(
+        marketplace.connect(buyer).buyNFT(tokenId, nftContractAddress, { value: smallerPrice })
+      ).to.be.revertedWith("Insufficient funds");
+    });
+  });
+
+  describe("Cancel", function () {
+    it("Should throw when trying to cancel NFT listing with not the owner", async function () {
+
+      let tokenId1 = 1;
+      await nftContract.safeMint(marketplace.address, owner.address);
+  
+      await marketplace
       .connect(owner)
-      .listNFT(tokenId, price, nftContractAddress);
-
-    const listing = await marketplace.listings(nftContractAddress, tokenId);
-    expect(listing.owner).to.equal(owner.address);
-    expect(listing.price).to.equal(price);
-
-    expect(await nftContract.ownerOf(tokenId)).to.equal(owner.address);
-
-    const filter = marketplace.filters.List();
-    const events = await marketplace.queryFilter(filter);
-
-    expect(events.length).to.equal(1);
+      .listNFT(tokenId1, price, nftContractAddress);
+  
+      await expect(
+        marketplace.connect(buyer).cancelListing(tokenId1, nftContractAddress)
+      ).to.be.revertedWith("Only owner can cancel the listing");
+    });
+  
+    it("Should throw when trying to cancel NFT listing that is not listed", async function () {
+      await expect(
+        marketplace.connect(owner).cancelListing(tokenId, nftContractAddress)
+      ).to.be.revertedWith("Token is not listed");
+    });
+  
+    it("Should cancel a listing", async function () {
+      await marketplace
+        .connect(owner)
+        .listNFT(tokenId, price, nftContractAddress);
+  
+      await marketplace.connect(owner).cancelListing(tokenId, nftContractAddress);
+  
+      expect(await nftContract.ownerOf(tokenId)).to.equal(owner.address);
+  
+      const filter = marketplace.filters.Cancel();
+      const events = await marketplace.queryFilter(filter);
+  
+      expect(events.length).to.equal(1);
+    })
   });
-
-  it("Should throw when listing an NFT that is already listed", async function () {
-    await marketplace
-      .connect(owner)
-      .listNFT(tokenId, price, nftContractAddress);
-
-    const listing = await marketplace.listings(nftContractAddress, tokenId);
-    expect(listing.owner).to.equal(owner.address);
-    expect(listing.price).to.equal(price);
-
-    expect(await nftContract.ownerOf(tokenId)).to.equal(owner.address);
-
-    expect(
-      marketplace.connect(owner).listNFT(tokenId, price, nftContractAddress)
-    ).to.be.revertedWith("NFT is already listed");
-  });
-
-  it("Should throw when listing an NFT with zero price", async function () {
-    expect(
-      marketplace.connect(owner).listNFT(tokenId, 0, nftContractAddress)
-    ).to.be.revertedWith("Price must be greater than zero");
-  });
-
-  it("Should buy an NFT", async function () {
-    await marketplace
-      .connect(owner)
-      .listNFT(tokenId, price, nftContractAddress);
-
-    const initialBalanceSeller = await ethers.provider.getBalance(
-      owner.address
-    );
-    const initialBalanceBuyer = await ethers.provider.getBalance(buyer.address);
-
-    const transaction = await marketplace
-      .connect(buyer)
-      .buyNFT(tokenId, nftContractAddress, { value: price });
-    const receipt = await transaction.wait();
-
-    const gasUsed = receipt.gasUsed;
-    const gasPrice = transaction.gasPrice;
-    const gasFee = gasUsed.mul(gasPrice);
-
-    const updatedBalanceSeller = await ethers.provider.getBalance(
-      owner.address
-    );
-    const updatedBalanceBuyer = await ethers.provider.getBalance(buyer.address);
-
-    expect(updatedBalanceSeller).to.equal(initialBalanceSeller.add(price));
-    expect(updatedBalanceBuyer).to.equal(
-      initialBalanceBuyer.sub(price).sub(gasFee)
-    );
-
-    expect(await nftContract.ownerOf(tokenId)).to.equal(buyer.address);
-
-    const filter = marketplace.filters.Buy();
-    const events = await marketplace.queryFilter(filter);
-
-    expect(events.length).to.equal(1);
-  });
-
-  it("Should throw when trying to buy NFT that is not listed", async function () {
-    await expect(
-      marketplace.connect(buyer).buyNFT(tokenId, nftContractAddress, { value: price })
-    ).to.be.revertedWith("Token is not listed");
-  });
-
-  it("Should throw when trying to buy NFT with unsufficient funds", async function () {
-    await marketplace
-      .connect(owner)
-      .listNFT(tokenId, price, nftContractAddress);
-
-    const smallerPrice = price.sub(ethers.utils.parseEther("0.1"));
-
-    await expect(
-      marketplace.connect(buyer).buyNFT(tokenId, nftContractAddress, { value: smallerPrice })
-    ).to.be.revertedWith("Insufficient funds");
-  });
-
-  it("Should throw when trying to cancel NFT listing with not the owner", async function () {
-
-    let tokenId1 = 1;
-    await nftContract.safeMint(marketplace.address, owner.address);
-
-    await marketplace
-    .connect(owner)
-    .listNFT(tokenId1, price, nftContractAddress);
-
-    await expect(
-      marketplace.connect(buyer).cancelListing(tokenId1, nftContractAddress)
-    ).to.be.revertedWith("Only owner can cancel the listing");
-  });
-
-  it("Should throw when trying to cancel NFT listing that is not listed", async function () {
-    await expect(
-      marketplace.connect(owner).cancelListing(tokenId, nftContractAddress)
-    ).to.be.revertedWith("Token is not listed");
-  });
-
-  it("Should cancel a listing", async function () {
-    await marketplace
-      .connect(owner)
-      .listNFT(tokenId, price, nftContractAddress);
-
-    await marketplace.connect(owner).cancelListing(tokenId, nftContractAddress);
-
-    expect(await nftContract.ownerOf(tokenId)).to.equal(owner.address);
-
-    const filter = marketplace.filters.Cancel();
-    const events = await marketplace.queryFilter(filter);
-
-    expect(events.length).to.equal(1);
-  })
-
 });
